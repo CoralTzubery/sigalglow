0759f741b3f903124cc5fac9e5813fca451e149e
Coral Tzubery <tzubery.coral@gmail.com>
2025-06-04 18:35:57 +0300

    adding cors

--- sigalglow-services/src/app.ts
+++ sigalglow-services/src/app.ts
@@ -7,7 +7,10 @@ import aboutRouter from "./routers/about.route";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://loacalhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/about", aboutRouter);
