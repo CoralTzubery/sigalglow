9ef73a2fcc96fcc0c10bc3cf65a56c64b7258354
Coral Tzubery <tzubery.coral@gmail.com>
2025-06-08 18:07:39 +0300

    adding a header to the treatments page

--- sigalglow-app/src/pages/Treatments.tsx
+++ sigalglow-app/src/pages/Treatments.tsx
@@ -7,6 +7,7 @@ export function Treatments() {
     return(
         <>
             <Main>
+                <h1>טיפולים</h1>
                 <section className={styles.grid}>
                     {treatments.map((treatment) => (
                         <TreatmentCard key={treatment.id} {...treatment}/>
