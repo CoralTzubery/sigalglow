dcfec8f73dfed336b2731425cffb7026e3c80ace
Coral Tzubery <tzubery.coral@gmail.com>
2025-06-10 20:12:50 +0300

    creating a contact model for the contact us page

---
+++ sigalglow-services/src/models/contact.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+const schema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true });
+
+export const Contact = model("Contact", schema);
